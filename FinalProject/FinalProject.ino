faae31747fd4e4682886638976362878bcf74f50
shuangshengjie <377311280@qq.com>
2019-05-19 01:46:03 +0800

    V 0.06
    根据安卓应用的流程更改了程序的流程，并删除了部分冗余的变量。在本版本中并未对传感器的数据进行更新。

--- FinalProject/FinalProject.ino
+++ FinalProject/FinalProject.ino
@@ -13,14 +13,13 @@ PQ_3:signal_High;   PQ_2:signal_Low;
 #include "TCS34725.h"
 #include "VL6180X.h"
 
-char buffer;	//data read from the Android board
-VL6180X distance(1);//data which is read by the distance sensor
-TCS34725 rgb(0);	//data which is read by the color sensor
-int colorJudged[4];	//the color data
-int DISTANCE_LOW = 16;	//the distance data	closest
-int DISTANCE_HIGH = 25;	//the distance data farest
+char buffer;	//从串口中读取到的数据
+VL6180X distance(1);//设置一个距离对象
+TCS34725 rgb(0);	//设置一个颜色对象
+int colorJudged[3];	//颜色传感器读取到的颜色数据
+int DISTANCE_LOW = 16;	//用户所在范围的最近距离
+int DISTANCE_HIGH = 25;	//用户所在范围的最远距离
 int userId = 0;
-int jumper;	//The value to control the program transport the data for only one time
 int TIME_LIMITED = 50;
 int score;
 
@@ -28,7 +27,7 @@ char userGroup[15] = {
 	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'
 };
 
-//Initial
+//初始化
 void setup() {
 	//与Android上位机通信的串口初始化
 	Serial3.begin(115200);
@@ -42,7 +41,6 @@ void setup() {
 	//红外距离传感器的输入引脚		胜负判断引脚
 	pinMode(PE_4, INPUT);
 	//与FPGA进行并行串口通信的端口 
-	pinMode(PQ_2, OUTPUT);//Give the ball
 	pinMode(PQ_3, OUTPUT);//HIGH
 	pinMode(PP_3, OUTPUT);
 	pinMode(PQ_1, OUTPUT);//LOW
@@ -54,12 +52,12 @@ void setup() {
 	pinMode(PL_3, INPUT);//LOW
 
 	pinMode(D1_LED, OUTPUT);
+	RESETON();
 }
 // 主循环
 void loop() {
-	JudgeId();
-	stayHere();			//判断用户距离
-	chooseMode();
+	JudgeId();//判断用户ID及维护模式
+	stayHere();//判断用户距离
 	delay(2000);
 }
 //读取用户ID
@@ -72,13 +70,24 @@ void JudgeId() {
 	int userIdGet2;
 	//Get the User ID
 	while (1) {
+		
+		if (Serial3.available() > 0) {
+			if (Serial3.read() == 'b') {
+				debugMode1();
+			}
+			else if (Serial3.read() == 'c') {
+				debugMode2();
+			}
+		}
+		Serial3.flush();
+
 		button1 = digitalRead(PL_0);
 		button2 = digitalRead(PL_1);
 		button3 = digitalRead(PL_2);
 		button4 = digitalRead(PL_3);
 
 		userIdGet1 = button1 * 8 + button2 * 4 + button3 * 2 + button4 * 1;
-		delay(800);
+		delay(300);
 
 		button1 = digitalRead(PL_0);
 		button2 = digitalRead(PL_1);
@@ -91,6 +100,8 @@ void JudgeId() {
 				break;
 			}
 		}
+
+		
 	}
 	userId = userIdGet1;
 	//Send the User ID
@@ -105,65 +116,10 @@ void JudgeId() {
 		delay(200);
 	}
 }
-//根据读取的串口数据进行模式的分流
-void chooseMode() {
-	colorFinalCheck();
-	while (1) {
-		if (Serial3.available() > 0) {
-			buffer = Serial3.read();
-			//普通游戏模式
-			if (buffer == 'd') {
-				NormalMode();
-				Serial3.flush();
-				chooseMode();//back to check wether the value has changed.
-			}
-			//调试模式1
-			if (buffer == 'b') {
-				debugMode1();
-				Serial3.flush();
-				chooseMode();//back to check wether the value has changed.
-			}
-			//调试模式2
-			if (buffer == 'c') {
-				debugMode2();
-				Serial3.flush();
-				chooseMode();//back to check wether the value has changed.
-			}
-			if (buffer == 'e') {
-				delay(2000);
-				break;
-			}
-			if (buffer == 'r') {
-				colorFinalCheck();
-			}
-		}
-	}
-}
-//和FPGA进行并行串口通信的四种基础模式
-//10输出绿色
-void setGreen() {
-	digitalWrite(PQ_3, HIGH);
-	digitalWrite(PQ_2, LOW);
-}
-//01输出蓝色
-void setBlue() {
-	digitalWrite(PQ_3, LOW);
-	digitalWrite(PQ_2, HIGH);
-}
-//00输出红色
-void setRed() {
-	digitalWrite(PQ_3, LOW);
-	digitalWrite(PQ_2, LOW);
-}
-//11全部停止 || 复位？
-void turnOff() {
-	digitalWrite(PQ_3, HIGH);
-	digitalWrite(PQ_2, HIGH);
-}
-//普通游戏模式			未完
+
+//正常的游戏模式
 void NormalMode() {
-	GiveBall();			//出球
-	delay(2000);
+	delay(3000);//根据最后的实际情况进行更改
 	colorFinalCheck();
 	while (1) {
 		if (Serial3.available() > 0) {
@@ -176,50 +132,65 @@ void NormalMode() {
 	}
 	colorSendtoFPGA();	//颜色判断&&控制舵机活动模式
 	sendDataToAndroid();//发送输赢数据给安卓
+	//根据上位机的返还数据决定是否结束游戏
+	while (1) {
+		if (Serial3.available() > 0) {
+			if (Serial3.read() == 'e') {
+				break;
+			}
+			else if (Serial3.read() == 'r') {
+				NormalMode();
+				break;
+			}
+		}
+	}
 }
-//更改输出引脚以及输出信号		更改
-void GiveBall() {
-	digitalWrite(PP_3, HIGH);
-	delay(1000);
-	digitalWrite(PP_3, LOW);
-}
+
 //颜色识别程序			未完		实际数据采样
 void colorSendtoFPGA() {
-	if (colorJudged[0] && colorJudged[1] && colorJudged[2] && colorJudged[3]) {
+	if (colorJudged[0] && colorJudged[1] && colorJudged[2]) {
 		setGreen();
 	}
-	else if (colorJudged[0] && colorJudged[1] && colorJudged[2] && colorJudged[3]) {
+	else if (colorJudged[0] && colorJudged[1] && colorJudged[2]) {
 		setRed();
 	}
-	else if (colorJudged[0] && colorJudged[1] && colorJudged[2] && colorJudged[3]) {
+	else if (colorJudged[0] && colorJudged[1] && colorJudged[2]) {
 		setBlue();
 	}
 }
 
 void colorFinalCheck() {
-	int* color1 = getColor();
-	int* color2 = getColor();
-	int* color3 = getColor();
-	int* color4 = getColor();
-	int* color5 = getColor();
+	int* color1 = getColor2();
+	int* color2 = getColor2();
+	int* color3 = getColor2();
+	int* color4 = getColor2();
+	int* color5 = getColor2();
 	int* coloraverage = average(color1, color2, color3, color4, color5);
 	colorJudged[0] = coloraverage[0];
 	colorJudged[1] = coloraverage[1];
 	colorJudged[2] = coloraverage[2];
-	colorJudged[3] = coloraverage[3];
 }
-int* getColor() {
-	int color[4];
+int* getColor2(){
+	int* color1 = getColor1();
+	int* color2 = getColor1();
+	int* color3 = getColor1();
+	int* color4 = getColor1();
+	int* color5 = getColor1();
+	int* coloraverage = average(color1, color2, color3, color4, color5);
+	return coloraverage;
+}
+int* getColor1() {
+	int color[3];
 	color[0] = rgb.getGreenData();
 	color[1] = rgb.getBlueData();
 	color[2] = rgb.getRedData();
-	color[3] = rgb.getClearData();
 	delay(100);
 	return color;
 }
+//对采集到的颜色数据进行平均化处理
 int* average(int* color1, int* color2, int* color3, int* color4, int* color5) {
-	int color[4];
-	for (int i = 0; i < 4; i++) {
+	int color[3];
+	for (int i = 0; i < 3; i++) {
 		color[i] = color1[i] + color2[i] + color3[i] + color4[i] + color5[i];
 		color[i] /= 5;
 	}
@@ -262,12 +233,12 @@ void sendDataToAndroid() {
 		delay(500);
 	}
 }
-//更改为积分制游戏
+//更改为积分制游戏		根据最后讨论的游戏时间进行调整
 int winORlose() {
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 100; j++) {
 			if (digitalRead(PE_4) == 0) {
-				score = ((600-i*j) / 500) * 100;
+				score = ((1000-i*j) / 1000) * 100;
 				return score;
 			}
 			delay(10);
@@ -278,31 +249,58 @@ int winORlose() {
 }
 //调试模式1：将信号发送给下位机使其逐个调用电机。
 void debugMode1() {
-	setGreen();
-	delay(1000);
-	setBlue();
-	delay(1000);
-	setRed();
-	delay(1000);
-	turnOff();
-	delay(5000);
+	RESETOFF();
+	delay(2000);
+	RESETON();
 }
 //调试模式2:将传感器读取到的数据传输给上位机
 void debugMode2() {
 	Serial3.print("R:");
-	Serial3.print(String(rgb.getRedData(), 10));
+	Serial3.print(rgb.getRedData());
 	Serial3.print("\tG:");
-	Serial3.print(String(rgb.getGreenData(), 10));
+	Serial3.print(rgb.getGreenData());
 	Serial3.print("\tB:");
-	Serial3.print(String(rgb.getBlueData(), 10));
+	Serial3.print(rgb.getBlueData());
 	Serial3.print("\tC:");
-	Serial3.println(String(rgb.getClearData(), 10));
+	Serial3.println(rgb.getClearData());
 	Serial3.print("Distance:");
-	Serial3.println(String(distance.readRangeSingle(), 10));
+	Serial3.println(distance.readRangeSingle());
 }
+//传输数据成功后亮灯提示
 void blink() {
 	digitalWrite(D1_LED, HIGH);
 	delay(200);
 	digitalWrite(D1_LED, LOW);
 	delay(200);
+}
+//和FPGA进行并行串口通信的四种基础模式		更改
+//设置FPGA的输出模式为绿色
+void setGreen() {
+	digitalWrite(PQ_3, LOW);
+	digitalWrite(PP_3, HIGH);
+	digitalWrite(PQ_1, LOW);
+}
+//设置FPGA的输出模式为蓝色
+void setBlue() {
+	digitalWrite(PQ_3, LOW);
+	digitalWrite(PP_3, HIGH);
+	digitalWrite(PQ_1, HIGH);
+}
+//设置FPGA的输出模式为红色
+void setRed() {
+	digitalWrite(PQ_3, LOW);
+	digitalWrite(PP_3, LOW);
+	digitalWrite(PQ_1, HIGH);
+}
+//将FPGA置为初始化模式
+void RESETON() {
+	digitalWrite(PQ_3, LOW);
+	digitalWrite(PP_3, LOW);
+	digitalWrite(PQ_1, LOW);
+}
+//将FPGA置为调试模式中舵机全部抬起的状态
+void RESETOFF() {
+	digitalWrite(PQ_3, HIGH);
+	digitalWrite(PP_3, LOW);
+	digitalWrite(PQ_1, LOW);
 }
